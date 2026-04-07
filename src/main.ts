5b52c5b51a0480ad9f7fb59cffdd8b976d225c16
Arman Mustamandyar <109197285+Armancollab@users.noreply.github.com>
2026-04-08 00:41:47 +0430

    the same change

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 require('dotenv').config();
 
-import Fastify from 'fastify';
+import fastify from 'fastify';
 import FastifyCors from '@fastify/cors';
-import type { FastifyInstance } from 'fastify';
 
 import anime from './routes/anime';
 import meta from './routes/meta';
@@ -10,37 +9,37 @@ import meta from './routes/meta';
 const start = async () => {
   const PORT = Number(process.env.PORT) || 3000;
   
-  const fastify: FastifyInstance = Fastify({
+  const app = fastify({
     logger: true,
   });
 
   try {
-    await fastify.register(FastifyCors, {
+    await app.register(FastifyCors, {
       origin: '*',
       methods: 'GET',
     });
 
-    await fastify.register(anime, { prefix: '/anime' });
-    await fastify.register(meta, { prefix: '/meta' });
+    await app.register(anime, { prefix: '/anime' });
+    await app.register(meta, { prefix: '/meta' });
 
-    fastify.get('/', async (_, reply) => {
+    app.get('/', async (request, reply) => {
       return reply.status(200).send({
         message: 'Welcome to consumet api!',
         routes: ['/anime', '/meta'],
       });
     });
 
-    fastify.get('*', async (_, reply) => {
+    app.get('*', async (request, reply) => {
       return reply.status(404).send({
         error: 'page not found',
         message: '',
       });
     });
 
-    await fastify.listen({ port: PORT, host: '0.0.0.0' });
+    await app.listen({ port: PORT, host: '0.0.0.0' });
     console.log(`Server listening on port ${PORT}`);
   } catch (err) {
-    fastify.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
 };
