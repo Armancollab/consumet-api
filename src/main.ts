1b8893b5df63c2ea061da901d39f43d16e98e9f7
Arman Mustamandyar <109197285+Armancollab@users.noreply.github.com>
2026-04-08 01:10:58 +0430

    revert the changes

--- src/main.ts
+++ src/main.ts
@@ -7,37 +7,44 @@ import anime from './routes/anime';
 import meta from './routes/meta';
 
 (async () => {
-  const PORT = Number(process.env.PORT) || 3000;
+  const PORT = Number(process.env.PORT);
   const fastify = Fastify({
     logger: true,
   });
-  
+
   await fastify.register(FastifyCors, {
     origin: '*',
     methods: 'GET',
   });
-  
+
   await fastify.register(anime, { prefix: '/anime' });
   await fastify.register(meta, { prefix: '/meta' });
 
   try {
-    fastify.get('/', (_, rp) => {
-      rp.status(200).send('Welcome to consumet api! 🎉');
-    });
-    
-    fastify.get('*', (request, reply) => {
-      reply.status(404).send({
-        message: '',
-        error: 'page not found',
+    fastify.get('/', (_, reply) => {
+      reply.status(200).send({
+        message: 'Welcome to consumet api!',
+        routes: ['/anime', '/meta'],
       });
     });
 
-    fastify.listen({ port: PORT, host: '0.0.0.0' }, (e, address) => {
-      if (e) throw e;
+    fastify.get('*', (_, reply) => {
+      reply.status(404).send({
+        error: 'page not found',
+        message: '',
+      });
+    });
+
+    fastify.listen({ port: PORT, host: '0.0.0.0' }, (error, address) => {
+      if (error) {
+        throw error;
+      }
+
       console.log(`server listening on ${address}`);
     });
-  } catch (err: any) {
-    fastify.log.error(err);
+  } catch (error) {
+    fastify.log.error(error);
     process.exit(1);
   }
 })();
+
